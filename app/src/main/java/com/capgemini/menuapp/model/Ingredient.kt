0bd982f950e8cad0b8a817605fb09ae51f5b6446
Mason <mason.kossen@capgemini.com>
2022-10-18 15:05:35 -0400

    Added serialized names.

--- app/src/main/java/com/capgemini/menuapp/model/Ingredient.kt
+++ app/src/main/java/com/capgemini/menuapp/model/Ingredient.kt
@@ -1,8 +1,14 @@
 package com.capgemini.menuapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Ingredient(
+    @SerializedName("ProductCode")
     val ProductCode: Int,
+    @SerializedName("MinQuantity")
     val MinQuantity: Int,
+    @SerializedName("MaxQuantity")
     val MaxQuantity: Int,
+    @SerializedName("DefaultQuantity")
     val DefaultQuantity: Int
 )
