6ec55ac60fc33a2e6d5445aa7a9df9f704938cd8
Dave-Murdoch <david.a.murdoch@capgemini.com>
2022-10-31 17:20:00 -0500

    Added a comment

--- app/src/main/java/com/capgemini/menuapp/view/MainActivity.kt
+++ app/src/main/java/com/capgemini/menuapp/view/MainActivity.kt
@@ -40,6 +40,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViewModel() {
+        // This will work, but using the built-in ViewModelProvider provides some optimizations.
+        //productListViewModel = ViewModelFactory().create(ProductListViewModel::class.java)
+
         productListViewModel = ViewModelProvider(this, ViewModelFactory())
             .get(ProductListViewModel::class.java)
     }
