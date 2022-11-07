a505e50ffe36607d69953ad2844b382f28ef94d2
Dave-Murdoch <david.a.murdoch@capgemini.com>
2022-11-07 13:58:13 -0600

    Demote subscribeOn and observeOn schedulers into Service impl
    Make Schedulers a concern of the Service Implementation and not individual ViewModels

--- app/src/main/java/com/capgemini/menuapp/service/ProductServiceImpl.kt
+++ app/src/main/java/com/capgemini/menuapp/service/ProductServiceImpl.kt
@@ -3,14 +3,16 @@ package com.capgemini.menuapp.service
 import com.capgemini.menuapp.model.Product
 import com.rx2androidnetworking.Rx2AndroidNetworking
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 
 class ProductServiceImpl : ProductService {
-
     override fun getProducts(): Single<List<Product>> {
-        return Rx2AndroidNetworking.get("https://63407b4fe44b83bc73d137ee.mockapi.io//api/v1/menu")
+        return Rx2AndroidNetworking.get("https://63407b4fe44b83bc73d137ee.mockapi.io/api/v1/menu")
             .build()
             .getObjectListSingle(Product::class.java)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
-
 }
