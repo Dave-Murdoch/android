a62e7a5aa1dda2e87509a55bfb4489b6b3fca342
aidanphelan <aidan.phelan@capgemini.com>
2022-11-08 10:38:30 -0600

    Implemented onBindViewHolder

--- app/src/main/java/com/capgemini/menuapp/view/ProductListViewAdapter.kt
+++ app/src/main/java/com/capgemini/menuapp/view/ProductListViewAdapter.kt
@@ -19,12 +19,21 @@ class ProductListViewAdapter(
     ): DataViewHolder {
         TODO("Check with other team for name of R.id.")
       // attachToRoot is false: RecyclerView attaches to the View Hierarchy at runtime
-        val view = LayoutInflater.from(context).inflate(R.id.item_layout,parent,false)
-        return DataViewHolder(view)
+        val productView = LayoutInflater.from(context).inflate(R.id.item_layout,parent,false)
+        return DataViewHolder(productView)
     }
 
-    override fun onBindViewHolder(holder: ProductListViewAdapter.DataViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    // Replaces contents of ListView
+    override fun onBindViewHolder(dataHolder: ProductListViewAdapter.DataViewHolder, position: Int) {
+
+        // Pull a product with a given position
+        val product = dataset[position]
+        // Set the data for a product/position
+        dataHolder.setData(product,position)
+
+        // Commented out until listeners are added
+        // dataHolder.setListeners()
+
     }
 
     override fun getItemCount(): Int {
