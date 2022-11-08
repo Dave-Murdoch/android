c1c70d9ad8c083aec53f4327b51b6fa733bd79e1
kokramer <kohlman.kramer@capgemini.com>
2022-11-08 16:01:50 -0600

    Added recycler view to first fragment. Defined item layout xml. Made recycler view visible in main activity.

--- app/src/main/java/com/capgemini/menuapp/view/MainActivity.kt
+++ app/src/main/java/com/capgemini/menuapp/view/MainActivity.kt
@@ -1,43 +1,62 @@
 package com.capgemini.menuapp.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.capgemini.menuapp.R
 import com.capgemini.menuapp.databinding.ActivityMainBinding
+import com.capgemini.menuapp.model.Product
 import com.capgemini.menuapp.viewmodel.ProductListViewModel
 import com.capgemini.menuapp.viewmodel.ViewModelFactory
+import com.capgemini.menuapp.view.ProductListViewAdapter
+import kotlinx.android.synthetic.main.fragment_first.*
+
 
 // TODO: Rename Activity to ProductListActivity
 class MainActivity : AppCompatActivity() {
     private lateinit var productListViewModel: ProductListViewModel
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ProductListViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
+        setContentView(R.layout.activity_main)
         setupViewModel()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun setupUI() {
+        LinearLayoutManager(this).also { recyclerView.layoutManager = it }
+        adapter = ProductListViewAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
     }
+
+    private fun setupObserver() {
+        productListViewModel.getProducts().observe(this, Observer {
+            renderList(it)
+            recyclerView.visibility = View.VISIBLE
+        })
+    }
+
+    private fun renderList(Products: List<Product>) {
+        adapter.addData(Products)
+        adapter.notifyDataSetChanged()
+    }
+
 
     private fun setupViewModel() {
         // This will work, but using the built-in ViewModelProvider provides some optimizations.
