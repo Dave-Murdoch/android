a7d8007b4285f0041d8c0ad8ee807a3a161fe28a
olivia-mason-star <114250963+olivia-mason-star@users.noreply.github.com>
2022-12-21 16:42:34 -0600

    live coding session with Dave. refactored MockProductServiceImpl to take in new products.json file.

--- app/src/test/java/com/capgemini/menuapp/MockProductServiceImpl.kt
+++ app/src/test/java/com/capgemini/menuapp/MockProductServiceImpl.kt
@@ -2,40 +2,18 @@ package com.capgemini.menuapp
 
 import com.capgemini.menuapp.model.*
 import com.capgemini.menuapp.service.ProductService
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import io.reactivex.Single
 import io.reactivex.*
 
-class MockProductServiceImpl: ProductService {
+class MockProductServiceImpl(private val file : String): ProductService {
 
     lateinit var prodList: MutableList<Product>
 
     override fun getProducts(): Single<List<Product>> {
-        val Product1 = Product(312, 1, 3,
-            listOf(Name("mock1", "en-US"), Name("mock2", "en-CA")),
-            listOf(Category(14,2), Category(12,9)),
-            Recipe(99, arrayOf(Ingredient(144,0,2,1),
-                            Ingredient(222,1,17,4)
-            )
-            )
-        )
-        val Product2 = Product(15, 1, 3,
-            listOf(Name("mock1", "en-US"), Name("mock2", "en-CA")),
-            listOf(Category(22,3), Category(35,15)),
-            Recipe(99, arrayOf(Ingredient(144,0,2,1),
-                Ingredient(222,1,17,4))
-            )
-        )
-
-        val Product3 = Product(11, 1, 3,
-            listOf(Name("mock1", "en-US"), Name("mock2", "en-CA")),
-            listOf(Category(8,1), Category(7,20)),
-            Recipe(99, arrayOf(Ingredient(144,0,2,1),
-                Ingredient(222,1,17,4))
-            )
-        )
-
-        prodList = mutableListOf(Product1,Product2,Product3)
-
+        val json = this::class.java.classLoader.getResource(file).readText()
+        prodList = Gson().fromJson(json, object : TypeToken<MutableList<Product>>(){}.type)
         return Single.just(prodList)
     }
 
--- app/src/test/java/com/capgemini/menuapp/ViewModelUnitTest.kt
+++ app/src/test/java/com/capgemini/menuapp/ViewModelUnitTest.kt
@@ -2,14 +2,26 @@ package com.capgemini.menuapp
 
 
 
+import androidx.annotation.NonNull
 import com.capgemini.menuapp.viewmodel.ProductDetailViewModel
 import com.capgemini.menuapp.viewmodel.ProductListViewModel
-import org.junit.Test
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.internal.schedulers.ExecutorScheduler.ExecutorWorker
+import io.reactivex.plugins.RxJavaPlugins
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.Callable
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
 
 class ViewModelUnitTest {
 
-    val mockProductRepo = MockProductRepoImpl(MockProductServiceImpl())
+    val mockProductRepo = MockProductRepoImpl(MockProductServiceImpl("/mocks/products.json"))
     val listViewModel = ProductListViewModel(mockProductRepo)
     val detailViewModel = ProductDetailViewModel(mockProductRepo)
 
@@ -29,5 +41,26 @@ class ViewModelUnitTest {
         assertEquals(products.value?.size, mockProductRepo.getProducts().flattenAsObservable { it }.count().blockingGet())
     }
 
-
+//    @Before
+//    fun setUpRxSchedulers() {
+//        val immediate: Scheduler = object : Scheduler() {
+//            override fun scheduleDirect(
+//                @NonNull run: Runnable,
+//                delay: Long,
+//                @NonNull unit: TimeUnit
+//            ): Disposable {
+//                // this prevents StackOverflowErrors when scheduling with a delay
+//                return super.scheduleDirect(run!!, 0, unit)
+//            }
+//
+//            override fun createWorker(): Worker {
+//                return ExecutorScheduler.ExecutorWorker(Runnable::run)
+//            }
+//        }
+//        RxJavaPlugins.setInitIoSchedulerHandler { scheduler: Callable<Scheduler?>? -> immediate }
+//        RxJavaPlugins.setInitComputationSchedulerHandler { scheduler: Callable<Scheduler?>? -> immediate }
+//        RxJavaPlugins.setInitNewThreadSchedulerHandler { scheduler: Callable<Scheduler?>? -> immediate }
+//        RxJavaPlugins.setInitSingleSchedulerHandler { scheduler: Callable<Scheduler?>? -> immediate }
+//        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler: Callable<Scheduler?>? -> immediate }
+//    }
 }
